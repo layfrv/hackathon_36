d48c8aae39b096537d96dbdd998c078b38e2e315
layfr <laylafirsova@gmail.com>
2023-11-11 00:36:32 +0700

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+function App() {
+  return (
+    <div className='app'>
+      <div className='app__header'>
+        <div className='app__header_menu'>
+        </div>
+      </div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+}
+export default App;
